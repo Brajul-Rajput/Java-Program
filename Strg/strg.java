33e5702230a9d07d7c9a3f41f83501feac6b2fd3
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-06-25 16:49:23 +0530

    Create strg.java

---
+++ Strg/strg.java
@@ -0,0 +1,11 @@
+package Strg;
+
+import java.util.*;
+public class strg {
+    public static void main(String[]args){
+        String name= new String();
+        name= "Hello, World!";
+        System.out.println(name);
+
+    }
+}
