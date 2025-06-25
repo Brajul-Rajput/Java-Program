8d500d4a97449d187cf2907f1b7218d32f01a812
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-06-25 16:48:39 +0530

    Create Arry1.java

---
+++ Arry/Arry1.java
@@ -0,0 +1,17 @@
+package Arry;
+
+public class Arry1{
+    public static void main(String[]args){
+        int []arr=new int[3];
+       arr[0]=10;
+        arr[1]=20;
+        arr[2]=30; 
+        //System.out.println(arr[0]);
+        //System.out.println(arr[1]);
+        //System.out.println(arr[2]);
+
+        for(int b:arr)
+        System.out.println(b);
+
+    }
+}
