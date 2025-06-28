f4aa428cf15acefcc188dfa0c6956b1c6ad88d03
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-06-28 10:59:32 +0530

    Create Program7.java

---
+++ BASIC/Program7.java
@@ -0,0 +1,12 @@
+package BASIC;
+public class Program7{
+public static void main(String[]args){
+    int a=10,b=20;
+    System.out.println("Addition of "+a+" and "+b+" is: "+(a+b));
+    System.out.println("Subtraction of "+a+" and "+b+" is: "+(a-b));
+    System.out.println("Multiplication of "+a+" and "+b+" is: "+(a*b));
+    System.out.println("Division of "+a+" and "+b+" is: "+(a/b));
+    System.out.println("Modulus of "+a+" and "+b+" is: "+(a%b));
+}   
+
+}
