f7f8c2877181e65fa2da9ddf2f1b80a10e784ae6
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-01 10:06:30 +0530

    Create program12.java

---
+++ BASIC/program12.java
@@ -0,0 +1,11 @@
+package BASIC;
+import java .util.Scanner;
+public class program12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(char i='A';i<='Z';i++){
+            System.out.print(i+" ");
+        }
+        sc.close();
+    }
+}
