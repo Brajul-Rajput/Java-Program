f2e3bc5f487910cf4d76a1df711bb5964aac325e
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-02 10:55:59 +0530

    Create p5.java

---
+++ PROBLEMS/p5.java
@@ -0,0 +1,10 @@
+package PROBLEMS;
+public class p5 {
+    public static void main(String[] args) {
+        
+        System.out.println("Decimal "+10);
+          System.out.println("Binary "+0B10);
+            System.out.println("octal "+010);
+              System.out.println("Hexadecimal  "+0x10);
+    }
+}
