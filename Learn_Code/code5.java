53d51b71164850778817875d234cda4ef12865ae
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-04 15:47:39 +0530

    Create code5.java

---
+++ Learn_Code/code5.java
@@ -0,0 +1,25 @@
+package Learn_Code;
+//Instance Blocks 
+class insta{
+    int x;
+    String y;
+   {
+        System.out.println(" first print in it ...");
+    }
+    
+    insta(int age ,String name){
+      x=age;
+     y=name;
+    }
+    void disp(){
+        System.out.println("second print : ");
+        System.out.println("Parametrized constructor : ");
+        System.out.println(x+" "+y);
+    }
+}
+public class code5 {
+    public static void main(String[] args) {
+        insta ref=new insta(21,"Kka");
+        ref.disp();
+    }
+}
