03371311deace3c7de7ea23f66fd651b16bc8a27
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-09 12:43:19 +0530

    Create Strng5.java

---
+++ Strg/Strng5.java
@@ -0,0 +1,15 @@
+package Strg;
+
+public class Strng5 {
+    public static void main(String[] args) {
+        
+    
+    String email1="shashwattiwari8@gmail.com";
+    String email2="random@.hotgmailcom";
+    System.out.println(email1.substring(16,21 ));
+    System.out.println(email2.substring(8, 16));
+   
+
+    }
+}
+ 
