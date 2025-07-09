1b0e3566e40d08a0208f26bae46c11f970934234
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-09 19:38:30 +0530

    Create ecepton.java

---
+++ OOPS/ecepton.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+public class ecepton {
+    public static void main(String[] args) {
+        int a=10;
+        int b=0;
+        int c;
+        try{
+c=a/b;
+System.out.println(c);
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+
+        }
+    }
+}
