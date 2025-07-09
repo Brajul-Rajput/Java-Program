681d40b35db71b68b096c30d1822a66abd834054
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-09 12:43:44 +0530

    Create Strng10.java

---
+++ Strg/Strng10.java
@@ -0,0 +1,20 @@
+package Strg;
+
+public class Strng10 {
+    public static void main(String[] args) {
+        String name="Brajul";
+
+        String str=" ";
+        for(int i=0;i<name.length();i++){
+        str=name.charAt(i)+str;
+        }
+        System.out.println(str);
+        String name2="naman";
+        
+
+    }
+
+}
+
+
+
