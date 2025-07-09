4a1b2399b577f0d66bff67a847324f6bb222972f
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-09 12:43:32 +0530

    Create String8.java

---
+++ Strg/String8.java
@@ -0,0 +1,9 @@
+package Strg;
+
+public class String8 {
+    public static void main(String[] args) {
+        double num=80.56489;
+    String sub=String.format("num is %.1f", num);
+    System.out.println(sub);
+    }
+}
