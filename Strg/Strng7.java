248813da41b07cf78539b05f50e641725f5ad48e
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-09 12:43:41 +0530

    Create Strng7.java

---
+++ Strg/Strng7.java
@@ -0,0 +1,9 @@
+package Strg;
+
+public class Strng7 {
+    public static void main(String[] args) {
+        System.out.println("brajul"+23+34.55);
+           System.out.println("brajul"+(23+34.55));
+        System.out.println(23+34.55+"brajul");
+    }
+}
