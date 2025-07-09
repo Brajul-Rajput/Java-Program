9e38daadae967cc878b7a07189347fe51f1c85e3
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-09 12:43:23 +0530

    Create Strng6.java

---
+++ Strg/Strng6.java
@@ -0,0 +1,15 @@
+package Strg;
+import java.util.Scanner;
+public class Strng6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         System.out.println("enter the word");
+        String word=sc.next();
+        System.out.println(word);
+        String c=sc.nextLine();
+        System.out.println("enter the line");
+        String line=sc.nextLine();
+        System.out.println(line);
+       
+    }
+}
