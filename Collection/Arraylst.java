b0737ad81b8e9d44b2d073d1c908ae2558e1886a
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-12 22:00:50 +0530

    Create Arraylst.java

---
+++ Collection/Arraylst.java
@@ -0,0 +1,12 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class Arraylst {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums=new ArrayList<>();
+        nums.add(10);
+        nums.add(20);
+        System.out.println(nums);
+    }
+}
