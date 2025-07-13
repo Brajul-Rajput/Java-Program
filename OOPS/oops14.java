7492547c1dd9707a52427e32738689889e7a0686
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-13 22:26:06 +0530

    Create oops14.java

---
+++ OOPS/oops14.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+public class oops14 {
+    public static void main(String[] args) {
+        A r=new A();
+        System.out.println(r);
+       r.how();
+    }
+}
+class A{
+void how(){
+    System.out.println(this);
+
+}
+}
