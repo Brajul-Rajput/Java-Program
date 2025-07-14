e0e5127f88ba2ade7bab72cb178bfaf7fd901e59
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-14 22:51:09 +0530

    Create student.java

---
+++ Project/src/main/java/org/example/student.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class student {
+  private  int studentid;
+   private String studentname;
+    private String studentaddress;
+
+
+
+    public int getStudentid() {
+        return studentid;
+    }
+
+    public String getStudentaddress() {
+        return studentaddress;
+    }
+
+    public String getStudentname() {
+        return studentname;
+    }
+}
