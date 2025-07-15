a24286c89495368d25467299a572ae95899f577c
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-15 12:23:39 +0530

    Create amazon.java

---
+++ ELC_BLC/amazon.java
@@ -0,0 +1,25 @@
+package ELC_BLC;
+
+public class amazon {
+    int id=10;
+    String name="Brajul rajput";
+    String phone="1234567890";
+    Long salary=100000L;
+    String password="Brajul@123";
+    String email="brajul@gmail.com";
+    String work="Software Engineer";
+    String address="India";
+    void info(){
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+     
+
+    }
+    void work(){
+   System.out.println("Email: " + email);
+        System.out.println("Work: " + work);
+        System.out.println("Address: " + address);
+    }
+
+}
