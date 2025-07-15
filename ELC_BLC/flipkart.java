a59b5401c858a860235106d884ce6290fd67fe64
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-15 12:23:46 +0530

    Create flipkart.java

---
+++ ELC_BLC/flipkart.java
@@ -0,0 +1,25 @@
+package ELC_BLC;
+
+public class flipkart {
+        int id=100;
+    String name="Brajul ";
+    String phone="1234567890";
+    Long salary=100000L;
+    String password="Brajul@123";
+    String email="brajul@gmail.com";
+    String work="Software Developer";
+    String address="Bangalore";
+    void info(){
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+     
+
+    }
+    void work(){
+   System.out.println("Email: " + email);
+        System.out.println("Work: " + work);
+        System.out.println("Address: " + address);
+    }
+
+}
