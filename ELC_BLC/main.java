a67732d7a68dd20bfda44b5f5c314cb0c7f6e571
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-15 12:23:49 +0530

    Create main.java

---
+++ ELC_BLC/main.java
@@ -0,0 +1,17 @@
+package ELC_BLC;
+
+public class main {
+   public static void main(String[] args) {
+    amazon re=new amazon();
+    re.info();
+    re.work();
+    flipkart re1=new flipkart();
+    re1.info();
+    re1.work();
+
+
+
+
+
+   }
+}
