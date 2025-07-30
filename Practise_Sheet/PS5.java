f202008fd4eed03854c75f9e3cf6d1c96f7fcd53
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-07-30 15:51:44 +0530

    Create PS5.java

---
+++ Practise_Sheet/PS5.java
@@ -0,0 +1,19 @@
+package Practise_Sheet;
+import java.util.Scanner;
+public class PS5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int count=0;
+        while(num>0){
+            int rem=num%10;
+            count++;
+            num = num / 10;
+        }
+        System.out.println("The digit count is "+count);
+        sc.close();
+    }
+}
+       
+    
