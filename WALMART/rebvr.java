8d0e977dd1b9d186758b27bfd39b359e7a7bd883
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-08-19 13:44:08 +0530

    Create rebvr.java

---
+++ WALMART/rebvr.java
@@ -0,0 +1,10 @@
+package WALMART;
+
+public class rebvr {
+    public static void main(String[] args) {
+        int []arr={12,13,14,15,16};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
