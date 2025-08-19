d5fa48955e80ec954698978e43e79f17ede23eca
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-08-19 13:44:10 +0530

    Create next.java

---
+++ WALMART/next.java
@@ -0,0 +1,10 @@
+package WALMART;
+
+public class next {
+    public static void main(String[] args) {
+        String num="5";
+     
+        int foo = Integer.parseInt(num);
+        System.out.println(foo*2);
+    }
+}
