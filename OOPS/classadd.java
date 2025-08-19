6dbd0fb758b6de167b675392c7bbf1e4746f839f
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-08-19 13:43:52 +0530

    Create classadd.java

---
+++ OOPS/classadd.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+public class classadd {
+    public static void main(String[] args) {
+      add.addit(2,3);
+       
+    }
+    
+}
+class add{
+    int a;
+    int b;
+  public static  int addit(int a,int b){
+      int  sum=a+b;
+       System.out.println(sum);
+       return sum;
+    }
+}
